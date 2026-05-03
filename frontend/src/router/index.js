48d3d761d67ebc1d3fe2e3ab683bcf7068cb499e
anianzyl <anian0510lzy@gmail.com>
2026-05-04 00:23:55 +0800

    初始化项目结构并导入前端代码

---
+++ frontend/src/router/index.js
@@ -0,0 +1,68 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { getToken } from "@/utils/storage.js";
+import echarts from 'echarts';
+Vue.prototype.$echarts = echarts;
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", redirect: "/login" },  // 临时重定向到登录页
+  // { path: "/", component: () => import(`@/views/category/Home.vue`) },
+  { path: "/login", component: () => import(`@/views/login/Login.vue`) },
+  { path: "/register", component: () => import(`@/views/register/Register.vue`) },
+  {
+    path: "/admin",
+    component: () => import(`@/views/admin/Home.vue`),
+    meta: { requireAuth: true },
+    children: [
+      { path: "/adminLayout", name: '仪表盘', icon: 'el-icon-pie-chart', component: () => import(`@/views/admin/Main.vue`), meta: { requireAuth: true } },
+      // { path: "/categoryManage", name: '用户管理', icon: 'el-icon-user-solid', component: () => import(`@/views/admin/category.vue`), meta: { requireAuth: true } },
+      // { path: "/categoryManage", name: '商品类别管理', icon: 'el-icon-document-checked', component: () => import(`@/views/admin/Category.vue`), meta: { requireAuth: true } },
+      // { path: "/evaluations", name: '评论管理', icon: 'el-icon-chat-dot-round', component: () => import(`@/views/admin/Evaluations.vue`), meta: { requireAuth: true } },
+    ]
+  },
+//   {
+//     path: "/category",
+//     component: () => import(`@/views/category/Home.vue`),
+//     meta: { requireAuth: true },
+//     children: [
+//       { name: '商品', path: "/product", component: () => import(`@/views/category/Product.vue`), meta: { requireAuth: true } },
+//       { name: '我的商品', path: "/myProduct", component: () => import(`@/views/category/MyProduct.vue`), meta: { requireAuth: true } },
+//       { name: '我的收藏', path: "/mySave", component: () => import(`@/views/category/MySave.vue`), meta: { requireAuth: true } },
+//       { name: '足迹', path: "/myView", component: () => import(`@/views/category/MyView.vue`), meta: { requireAuth: true } },
+//       { name: '订单', path: "/orders", component: () => import(`@/views/category/Orders.vue`), meta: { requireAuth: true } },
+//       { name: '消息', path: "/message", component: () => import(`@/views/category/Message.vue`), meta: { requireAuth: true } },
+//       { name: '个人中心', path: "/myself", component: () => import(`@/views/category/Myself.vue`), meta: { requireAuth: true } },
+//       { name: '搜索页', path: "/search", show: false, auth: false, component: () => import(`@/views/category/Search.vue`), meta: { requireAuth: true } },
+//       { name: '发布商品', path: "/post-product", component: () => import(`@/views/category/PostProduct.vue`), meta: { requireAuth: true } },
+//     ]
+//   }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    const token = getToken();
+    // 需要放行的路由
+    const releasePaths = ['/main', '/product'];
+    if (releasePaths.includes(to.path)) {
+      next();
+    } else {
+      if (token !== null) {
+        next();
+      } else {
+        next("/login");
+      }
+    }
+  }
+  else {
+    next();
+  }
+});
+export default router;
---
+++ frontend/src/views/user/PostProduct.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="post-product">
+    <div>
+        <input class="dialog-input" v-model="data.name" placeholder="商品名" />
+    </div>
+    <div>
+        
+    </div>
+    <div></div>
+   </div>
+</template>
+<script>
+export default {
+    name: 'PostProduct',
+    data() {
+        return {
+            product: {
+
+            }
+        };
+    },
+    created() {
+
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang="scss">
+.post-product{
+    display: flex;
+    justify-content: left;
+}
+</style>
