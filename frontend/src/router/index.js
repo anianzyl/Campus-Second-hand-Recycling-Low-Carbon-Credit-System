d0fc39bd2966bf9c82b8ae76d39c372db916fe24
anianzyl <anian0510lzy@gmail.com>
2026-05-08 17:22:01 +0800

    我的商品及商品编辑、删除

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
     meta: { requireAuth: true },
     children: [
       { path: "/adminLayout", name: '仪表盘', icon: 'el-icon-pie-chart', component: () => import(`@/views/admin/Main.vue`), meta: { requireAuth: true } },
-      // { path: "/categoryManage", name: '用户管理', icon: 'el-icon-user-solid', component: () => import(`@/views/admin/category.vue`), meta: { requireAuth: true } },
+      { path: "/categoryManage", name: '用户管理', icon: 'el-icon-user-solid', component: () => import(`@/views/admin/category.vue`), meta: { requireAuth: true } },
       { path: "/productManage", name: '商品管理', icon: 'el-icon-shopping-bag-1', component: () => import(`@/views/admin/Product.vue`), meta: { requireAuth: true } },
       { path: "/categoryManage", name: '商品类别管理', icon: 'el-icon-document-checked', component: () => import(`@/views/admin/Category.vue`), meta: { requireAuth: true } },
       // { path: "/evaluations", name: '评论管理', icon: 'el-icon-chat-dot-round', component: () => import(`@/views/admin/Evaluations.vue`), meta: { requireAuth: true } },
@@ -39,6 +39,7 @@ const routes = [
       { name: '个人中心', path: "/myself", component: () => import(`@/views/category/Myself.vue`), meta: { requireAuth: true } },
       { name: '搜索页', path: "/search", show: false, auth: false, component: () => import(`@/views/category/Search.vue`), meta: { requireAuth: true } },
       { name: '发布商品', path: "/post-product", component: () => import(`@/views/category/PostProduct.vue`), meta: { requireAuth: true } },
+      { name: '编辑商品', path: "/edit-product", component: () => import(`@/views/user/EditProduct.vue`), meta: { requireAuth: true } },
       { name: '商品详情', path: "/product-detail", component: () => import(`@/views/user/ProductDetail.vue`), meta: { requireAuth: true } },
     ]
   }
--- frontend/src/utils/storage.js
+++ frontend/src/utils/storage.js
@@ -1,6 +1,16 @@
 const TOKEN_KEY = "token"
 const USER_INFO = "userInfo";
 const SEARCH_KEY = "searchKey";
+const PRODUCT_INFO = "productInfo";
+//获取商品信息
+export function getProductInfo() {
+    const jsonProduct = sessionStorage.getItem(PRODUCT_INFO);
+    return JSON.parse(jsonProduct);
+}
+//存储商品信息
+export function setProductInfo(info) {
+    sessionStorage.setItem(PRODUCT_INFO, JSON.stringify(info));
+}
 //获取搜索关键词
 export function getSearchKey() {
     return sessionStorage.getItem(SEARCH_KEY);
@@ -9,12 +19,12 @@ export function getSearchKey() {
 export function setSearchKey(key) {
     sessionStorage.setItem(SEARCH_KEY, key);
 }
-//获取商品类别信息
+//获取用户信息
 export function getUserInfo() {
     const jsonUserInfo = sessionStorage.getItem(USER_INFO);
     return JSON.parse(jsonUserInfo);
 }
-//存储商品类别信息
+//存储用户信息
 export function setUserInfo(userInfo) {
     sessionStorage.setItem(USER_INFO, JSON.stringify(userInfo));
 }
