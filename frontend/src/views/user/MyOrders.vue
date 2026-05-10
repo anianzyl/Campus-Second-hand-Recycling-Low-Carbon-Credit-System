9834a662829afa4a5608a38d81720ea29a12af8e
anianzyl <anian0510lzy@gmail.com>
2026-05-10 10:01:06 +0800

    订单列表1

---
+++ frontend/src/views/user/MyOrders.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        我的订单页面
+    </div>
+</template>
+<script>
+export default {
+    name: 'MyOrders',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="scss">
+</style>
