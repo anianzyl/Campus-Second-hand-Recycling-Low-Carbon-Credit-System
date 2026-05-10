a293c1330fe0d8af11f311396edbcf37274753bf
Loonght666 <3335860259@qq.com>
2026-05-10 23:02:52 +0800

    ui: 完成“消息管理”页面前端布局与代码

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -23,6 +23,7 @@ const routes = [
       { path: "/productManage", name: '商品管理', icon: 'el-icon-shopping-bag-1', component: () => import(`@/views/admin/Product.vue`), meta: { requireAuth: true } },
       { path: "/ordersManage", name: '订单管理', icon: 'el-icon-document-copy', component: () => import(`@/views/admin/Orders.vue`), meta: { requireAuth: true } },
       { path: "/categoryManage", name: '商品类别管理', icon: 'el-icon-document-checked', component: () => import(`@/views/admin/Category.vue`), meta: { requireAuth: true } },
+      { path: "/messageManage", name: '消息管理', icon: 'el-icon-chat-square', component: () => import(`@/views/admin/Message.vue`), meta: { requireAuth: true } },
       // { path: "/evaluations", name: '评论管理', icon: 'el-icon-chat-dot-round', component: () => import(`@/views/admin/Evaluations.vue`), meta: { requireAuth: true } },
     ]
   },
